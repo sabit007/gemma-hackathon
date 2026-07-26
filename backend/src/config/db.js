48d5504f19710665003f2e4fdd0fbba6d62bc1ac
Napro20 <nafiul30p@gmail.com>
2026-07-26 16:35:25 +0600

    chore: disable automatic db seeder

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
-    await seedDatabase();
+    // await seedDatabase();
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
